23be1646bd35e0dd7336b67db1a1c6f9233449a8
Wesley F. Young <willamwei@outlook.com>
2024-07-03 20:29:02 +0800

    update: sync code in old repository

--- scripts/main.ts
+++ scripts/main.ts
@@ -3,4 +3,36 @@ import { StdhubPluginApi } from 'stdhub-plugin-api';
 export const pluginName = process.env.PLUGIN_NAME!;
 export const api = new StdhubPluginApi(pluginName);
 
-// Code below freely!
+import { system, world } from '@minecraft/server';
+
+api.readRootConfig({
+  commandPrefix: '.'
+}).then(({ commandPrefix }) => {
+  world.beforeEvents.chatSend.subscribe(event => {
+    if (!event.message.startsWith(commandPrefix)) {
+      return;
+    }
+
+    const commandString = event.message.slice(commandPrefix.length);
+    const [ commandName ] = commandString.split(' ', 2);
+
+    // The system.run is important here!
+    // You cannot call any async functions in the event handling scope.
+    system.run(() => {
+      api.postJson(
+        `${api.backendAddress}/command/submit`,
+        {
+          playerId: event.sender.id,
+          playerName: event.sender.name,
+          commandString
+        }
+      ).then(({ response: { status } }) => {
+        if (status === 404) {
+          event.sender.sendMessage(
+            `§cUnknown command: ${commandName}. Please check that the command exists and you have permission to use it.`
+          );
+        }
+      });
+    });
+  });
+});
